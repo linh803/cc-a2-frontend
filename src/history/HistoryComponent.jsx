95fdecd5eaf9a804f6ac9e04fe5483771447ad83
linh803 <phamphuonglinh2303@gmail.com>
2019-10-18 10:52:10 +1100

    Fix up page layout

--- src/history/HistoryComponent.jsx
+++ src/history/HistoryComponent.jsx
@@ -43,7 +43,7 @@ class HistoryComponent extends React.Component {
         if (VIDEOS[country] != null) {
             for (let id in VIDEOS[country]) {
                 content.push(
-                    <div key={id}>
+                    <div key={id} className="col-lg-6 mt-2 mb-2">
                         <Link to=
                             {{
                                 pathname: `/history/${id}`,
@@ -52,19 +52,19 @@ class HistoryComponent extends React.Component {
                                 }
                             }}
                         >
-                            <div className="row">
-                                <div className="col">
-                                    <iframe
-                                        title={id}
-                                        width="560" height="315"
-                                        src={`https://www.youtube.com/embed/${id}`}
-                                        frameBorder="0"
-                                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                        allowFullScreen
-                                    >
-                                    </iframe>
+                            <div className="card">
+                                <iframe className="card-img-top"
+                                    title={id}
+                                    width="560" height="315"
+                                    src={`https://www.youtube.com/embed/${id}`}
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                >
+                                </iframe>
+                                <div className="card-body">
+                                    <div className="card-text">{VIDEOS[country][id].name}</div>
                                 </div>
-                                <div className="col">{VIDEOS[country][id].name}</div>
                             </div>
                         </Link>
                     </div>
@@ -74,17 +74,25 @@ class HistoryComponent extends React.Component {
 
         return (
             <div>
-                {/*Form*/}
-                <form  onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <input type="text" className="form-control form-control-lg" placeholder="Enter a country :)" value={this.state.form.country} onChange={this.handleChange} />
+                <div className="container-fluid">
+                    <form onSubmit={this.handleSubmit}>
+                        <div class="form-row">
+                            <div className="col">
+                                <input type="text" className="form-control form-control-lg mr-2" placeholder="Enter a country :)" value={this.state.form.country} onChange={this.handleChange} />
+                            </div>
+
+                            <div className="col">
+                                <button type="submit" className="btn btn-primary btn-lg">Show Videos</button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        {content}
                     </div>
-
-                    <button type="submit" className="btn btn-primary">Search</button>
-                </form>
-
-                {/*All trending videos, sorted by views; // TODO: render by search*/}
-                {content}
+                </div>
             </div>
         );
     }
